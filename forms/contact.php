cf8642377cbd39a55b435685ff79c68e7f16ac4d
EduardTuros <eduardturos@gmail.com>
2021-11-03 16:22:44 +0000

    sss

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.sendgrid.net',
+    'username' => 'apikey',
+    'password' => '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',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
